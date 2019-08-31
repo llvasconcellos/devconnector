23586b02f7b956fd874edb84cd8a4c6f3a095760
Leonardo Lima de Vasconcellos <leonardo@devhouse.com.br>
2019-08-31 20:03:30 -0300

    setup reload browser reloading

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const reload = require("reload");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send('Hello Leonardo<script src="/reload/reload.js"></script>');
 });
 
-const port = process.env.PORT || 5000;
+app.set("port", process.env.PORT || 3000);
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+//app.listen(port, () => console.log(`Server running on port ${port}`));
+
+reload(app)
+  .then(function(reloadReturned) {
+    // reloadReturned is documented in the returns API in the README
+
+    // Reload started, start web server
+    app.listen(app.get("port"), function() {
+      console.log(`Web server listening on port ${app.get("port")}`);
+    });
+  })
+  .catch(function(err) {
+    console.error(
+      "Reload could not start, could not start server/sample app",
+      err
+    );
+  });
