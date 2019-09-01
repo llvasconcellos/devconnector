a90e902f559a6bcaf9aa7253ff046556e93da278
Leonardo Lima de Vasconcellos <leonardo@devhouse.com.br>
2019-09-01 14:13:18 -0300

    passport config with jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require("passport");
+
+//Load Input Validation
+const ValidateRegisterInput = require("../../validation/register");
+const ValidateLoginInput = require("../../validation/login");
+
+//Load User model
+const User = require("../../models/User");
 
 /**
  * @route       GET api/users/test
@@ -8,4 +20,109 @@ const router = express.Router();
  */
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 
+/**
+ * @route       POST api/users/register
+ * @description Register user
+ * @access      Public
+ */
+router.post("/register", (req, res) => {
+  const { errors, isValid } = ValidateRegisterInput(req.body);
+
+  //Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //Size
+        r: "pg", //Rating
+        d: "mm" //Default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.error(err));
+        });
+      });
+    }
+  });
+});
+
+/**
+ * @route       POST api/users/login
+ * @description Login User / Return JWT Token
+ * @access      Public
+ */
+router.post("/login", (req, res) => {
+  const { errors, isValid } = ValidateLoginInput(req.body);
+
+  //Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Find user by email
+  User.findOne({ email }).then(user => {
+    //check for user
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    //check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        jwt.sign(
+          {
+            id: user.id,
+            name: user.name,
+            avatar: user.avatar
+          },
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({ success: true, token });
+          }
+        );
+      } else {
+        return res.status(400).json({ password: "Password incorrect" });
+      }
+    });
+  });
+});
+
 module.exports = router;
+
+/**
+ * @route       POST api/users/current
+ * @description Return current user
+ * @access      Private
+ */
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const reload = require("reload");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -8,19 +10,28 @@ const posts = require("./routes/api/posts");
 
 const app = express();
 
-//DB Config
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+//DB Config
 const db = require("./config/keys").mongoURI;
 
 //Connect to mongoDB
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+  .catch(err => console.error(err));
 
 app.get("/", (req, res) => {
   res.send('Hello Leonardo<script src="/reload/reload.js"></script>');
 });
+
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport Config
+require("./config/passport")(passport);
 
 //Use Routes
 app.use("/api/users", users);
